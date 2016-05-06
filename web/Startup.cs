ba50e9e9fa40de855635c5a4194d587b516d8d01
geobarteam <gvd8@hotmail.com>
2016-05-06 12:58:11 +0200

    make routing working

--- web/Startup.cs
+++ web/Startup.cs
@@ -54,6 +54,8 @@ namespace web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute("spa-routes", "{*anything}", new { controller = "Home", action = "Index" });
             });
         }
 
