64cbc0430a8b076ec1e6e4be58c70f0fa7eb5a5d
geobarteam <gvd8@hotmail.com>
2016-05-21 12:38:48 +0200

    Integrated MongoDB with Repositories

---
+++ src/web/Data/Repositories/IEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace MyClub.Web.Data.Repositories
+{
+    /// <summary>
+    /// Generic Entity interface.
+    /// </summary>
+    /// <typeparam name="TKey">The type used for the entity's Id.</typeparam>
+    public interface IEntity<TKey>
+    {
+        /// <summary>
+        /// Gets or sets the Id of the Entity.
+        /// </summary>
+        /// <value>Id of the Entity.</value>
+        [BsonId]
+        TKey Id { get; set; }
+    }
+
+    /// <summary>
+    /// "Default" Entity interface.
+    /// </summary>
+    /// <remarks>Entities are assumed to use strings for Id's.</remarks>
+    public interface IEntity : IEntity<string>
+    {
+    }
+}
---
+++ src/web/Data/Repositories/MongoDB/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyClub.Web.Data.Repositories.MongoDB
+{
+    /// <summary>
+    /// Abstract Entity for all the BusinessEntities.
+    /// </summary>
+    [BsonIgnoreExtraElements(Inherited = true)]
+    public abstract class Entity : IEntity<string>
+    {
+        /// <summary>
+        /// Gets or sets the id for this object (the primary record for an entity).
+        /// </summary>
+        /// <value>The id for this object (the primary record for an entity).</value>
+        [BsonRepresentation(BsonType.ObjectId)]
+        public virtual string Id { get; set; }
+    }
+}
